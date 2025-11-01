fbee739a7f180aea4aabe22ef24be3c61f88838b
snts7kxx <90925892+snts7kxx@users.noreply.github.com>
2025-11-01 11:01:32 -0300

    Atualizar o SCRIPT.js

--- SCRIPT.js
+++ SCRIPT.js
@@ -142,8 +142,8 @@ function setupMain() {
             "radio 1": {
               type: "radio",
               options: {
-                choices: [{ content: "Correta", correct: true }]
-choices: [{ content: "Incorreta", correct: false }]
+                choices: [{ content: "Correta", correct: true }
+{ content: "Incorreta", correct: false }]
               }
             }
           };
