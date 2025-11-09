479a09a55034dd0e3d5a60710afb1336d67ee110
Matheus <90925892+snts7kxx@users.noreply.github.com>
2025-11-09 17:00:51 -0300

    Atualizar o SCRIPT.js

--- SCRIPT.js
+++ SCRIPT.js
@@ -56,7 +56,7 @@ async function showSplashScreen() {
   splashScreen.style.cssText = "position:fixed;top:0;left:0;width:100%;height:100%;background-color:#000;display:flex;align-items:center;justify-content:center;z-index:9999;opacity:0;transition:opacity 1,5s ease;user-select:none;color:white;font-family:MuseoSans,sans-serif;font-size:40px;text-align:center;";
 
 // Tela inicial
-  splashScreen.innerHTML = '<span style="color:white;text-shadow: 0 0 2px rgba(255,255,255,0.6);"><strong>KHAN</strong><span style="color:#af00ff;text-shadow: 0 0 2px rgba(255,255,255,0.6);"><strong>DARK</strong>';
+  splashScreen.innerHTML = '<span style="color:white;text-shadow: 0 0 2px rgba(255,255,255,0.67);"><strong>KHAN</strong><span style="color:#af00ff;text-shadow: 0 0 2px rgba(255,255,255,0.67);"><strong>DARK</strong>';
   document.body.appendChild(splashScreen);
   setTimeout(() => splashScreen.style.opacity = '1', 10);
 }
