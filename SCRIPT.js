3a85cc7f58ca4129f1084151573b93eb5511f9de
Matheus <90925892+snts7kxx@users.noreply.github.com>
2025-11-22 10:41:04 -0300

    Atualizar o SCRIPT.js

--- SCRIPT.js
+++ SCRIPT.js
@@ -357,7 +357,7 @@ if (!/^https?:\/\/([a-z0-9-]+\.)?khanacademy\.org/.test(window.location.href)) {
     await hideSplashScreen();
 
     setupMain();
-    sendToast("💜 | Khan Dark iniciado!");
+    sendToast("🔧 | Khan Manutenção iniciado!");
     console.clear();
   })();
 }
