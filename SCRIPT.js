8aa38b7848b314b32ac6077789dc87fcb331ec1b
Matheus <90925892+snts7kxx@users.noreply.github.com>
2025-11-23 22:33:12 -0300

    Atualizar o SCRIPT.js

--- SCRIPT.js
+++ SCRIPT.js
@@ -90,7 +90,7 @@ async function showSplashScreen() {
     splashScreen.innerHTML = `
         <div class="splash-content">
             <div>
-                <span style="color:white;">KHAN</span><span style="color:#af00ff;">DARK</span>
+                <span style="color:white;"><strong>KHAN</span><span style="color:#af00ff;"><strong>DARK</span>
             </div>
             <div class="loader-ring"></div>
             <div class="loading-text">Carregando módulos...</div>
